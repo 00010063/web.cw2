fc865a4cb3919bb95ad962e09d30f0f25ed0c7dd
00010063 <jamiwiut@mail.ru>
2021-04-06 01:22:26 +0500

    designing create form
    tried to make create form on the center but it didn't work
    
    and added backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+
 const fs = require('fs')
 
-
 app.set('view engine', 'pug')
+
 app.use('/static', express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
@@ -12,8 +13,19 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-
 app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const title = req.body.title
+    const description = req.body.description
+
+    if (title.trim() === '' && description.trim() === '') {
+        res.render('create', { error: true, msg: 'You should not submit empty title' })
+    }
+
     res.render('create')
 })
 
@@ -25,6 +37,10 @@ app.get('/notes', (req, res) => {
 
 app.get('/notes/details', (req, res) => {
     res.render('details')
+})
+
+app.get('/posts', (req, res) => {
+    res.render('posts')
 })
 
 app.listen(8000, err => {
