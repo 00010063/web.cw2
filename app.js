9db75701bda5d15e475c65179e301efc14a2734b
00010063 <jamiwiut@mail.ru>
2021-04-05 12:30:30 +0500

    creating page create
    and desgining the home page

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ app.set('view engine', 'pug')
 app.use('/static', express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
+//localhost:8000
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+
+app.get('/create', (req, res) => {
+    res.render('create')
 })
 
 app.listen(8000, err => {
